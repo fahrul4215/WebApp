4e52072811d775a6440c9e7a5ec93d199fb79d78
fahrul4215 <fahrul4215@gmail.com>
2019-06-26 23:36:15 +0700

    ngrapiin coding

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-   <Title>Registration Form</Title>
+   <Title>Form Registrasi Dicoding</Title>
    <style type="text/css">
       body { background-color: #fff; border-top: solid 10px #000;
           color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -19,68 +19,86 @@
    <h1>Register here!</h1>
    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
    <form method="post" action="index.php" enctype="multipart/form-data" >
-     Name  <input type="text" name="name" id="name"/></br></br>
-     Email <input type="text" name="email" id="email"/></br></br>
-     Job <input type="text" name="job" id="job"/></br></br>
-     <input type="submit" name="submit" value="Submit" />
-     <input type="submit" name="load_data" value="Load Data" />
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>:</td>
+            <td><input type="text" name="name" id="name"/></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td><input type="text" name="email" id="email"/></td>
+        </tr>
+        <tr>
+            <td>Job</td>
+            <td>:</td>
+            <td><input type="text" name="job" id="job"/></td>
+        </tr>
+        <tr>
+            <td rowspan="3">
+                <input type="submit" name="submit" value="Submit" />
+                <input type="submit" name="load_data" value="Load Data" />
+            </td>
+        </tr>
+    </table>     
  </form>
  <?php
- $host = "fahrul4215.database.windows.net";
- $user = "fahrul4215";
-        $pass = "Aris4215";
-            $db = "Pegawai";
-                try {
-                $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
-                $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-            } catch(Exception $e) {
+    $host = "fahrul4215.database.windows.net";
+    $user = "fahrul4215";
+    $pass = "Aris4215";
+    $db = "Pegawai";
+
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
+    if (isset($_POST['submit'])) {
+        try {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
+            // Insert data
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) VALUES (?,?,?,?)";
+            $stmt = $conn->prepare($sql_insert);
+            $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
+            $stmt->execute();
+        } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-        if (isset($_POST['submit'])) {
+        echo "<h3>Your're registered!</h3>";
+    } else if (isset($_POST['load_data'])) {
         try {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $job = $_POST['job'];
-        $date = date("Y-m-d");
-        // Insert data
-        $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-        VALUES (?,?,?,?)";
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $name);
-        $stmt->bindValue(2, $email);
-        $stmt->bindValue(3, $job);
-        $stmt->bindValue(4, $date);
-        $stmt->execute();
-    } catch(Exception $e) {
-    echo "Failed: " . $e;
-}
-echo "<h3>Your're registered!</h3>";
-} else if (isset($_POST['load_data'])) {
-try {
-$sql_select = "SELECT * FROM Registration";
-$stmt = $conn->query($sql_select);
-$registrants = $stmt->fetchAll(); 
-if(count($registrants) > 0) {
-echo "<h2>People who are registered:</h2>";
-echo "<table>";
-    echo "<tr><th>Name</th>";
-        echo "<th>Email</th>";
-        echo "<th>Job</th>";
-        echo "<th>Date</th></tr>";
-        foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-            echo "<td>".$registrant['email']."</td>";
-            echo "<td>".$registrant['job']."</td>";
-            echo "<td>".$registrant['date']."</td></tr>";
+            $sql_select = "SELECT * FROM Registration";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
         }
-    echo "</table>";
-} else {
-echo "<h3>No one is currently registered.</h3>";
-}
-} catch(Exception $e) {
-echo "Failed: " . $e;
-}
-}
-?>
+    }
+ ?>
 </body>
 </html>
